2ff4881c8f971f02722544fdb79ee1f5f956e941
Marc Philipp <mail@marcphilipp.de>
2017-03-02 12:39:45 +0100

    fixup! Make custom @ArgumentsSource annotations non-repeatable for now

--- junit-jupiter-params/src/main/java/org/junit/jupiter/params/sources/MethodSource.java
+++ junit-jupiter-params/src/main/java/org/junit/jupiter/params/sources/MethodSource.java
@@ -12,7 +12,6 @@ package org.junit.jupiter.params.sources;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
