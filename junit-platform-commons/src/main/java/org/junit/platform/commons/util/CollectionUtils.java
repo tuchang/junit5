342bc3795d444a7dd04a5822f9a49bcc208c86d9
Marc Philipp <mail@marcphilipp.de>
2017-03-06 22:17:38 +0100

    Support Object arrays in CollectionUtils.toStream

--- junit-platform-commons/src/main/java/org/junit/platform/commons/util/CollectionUtils.java
+++ junit-platform-commons/src/main/java/org/junit/platform/commons/util/CollectionUtils.java
@@ -15,6 +15,7 @@ import static java.util.Spliterators.spliteratorUnknownSize;
 import static java.util.stream.StreamSupport.stream;
 import static org.junit.platform.commons.meta.API.Usage.Internal;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.stream.Stream;
@@ -62,7 +63,7 @@ public final class CollectionUtils {
 	 * @param object the object to convert into a stream
 	 * @return the resulting stream
 	 * @throws PreconditionViolationException if the object is neither a Stream,
-	 * a Collection, an Iterable, nor an Iterator
+	 * a Collection, an Iterable, an Iterator, nor an Object Array.
 	 */
 	public static Stream<?> toStream(Object object) {
 		if (object instanceof Stream) {
@@ -76,6 +77,9 @@ public final class CollectionUtils {
 		}
 		if (object instanceof Iterator) {
 			return stream(spliteratorUnknownSize((Iterator<?>) object, ORDERED), false);
+		}
+		if (object instanceof Object[]) {
+			return Arrays.stream((Object[]) object);
 		}
 		throw new PreconditionViolationException(
 			"Cannot convert instance of " + object.getClass().getName() + " into a Stream: " + object);
